6a6a349a91b71a07647bfe2abe7e715321e27648
arnaud thiebart <arno.thieb@gmail.com>
2022-12-19 17:39:17 +0100

    model + controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'bookmarks/new'
+  get 'bookmarks/create'
+  get 'bookmarks/destroy'
+  get 'lists/index'
+  get 'lists/new'
+  get 'lists/create'
+  get 'lists/show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
