73bfe24edb13a5c37003a601012df77fddb1c7d3
arnaud thiebart <arno.thieb@gmail.com>
2022-12-20 14:41:18 +0100

    model et controller et routes terminer

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,2 +1,8 @@
 class Bookmark < ApplicationRecord
+  belongs_to :movies, dependent: :destroy
+  belongs_to :lists
+
+  validates :comment, length: { minimum: 6 }
+  validates_associated :movie_id, :list_id, uniqueness: true
+  # validates :movie_id, uniqueness: { scope: list_id, message: "is already in the list" }
 end
--- app/models/list.rb
+++ app/models/list.rb
@@ -1,2 +1,8 @@
 class List < ApplicationRecord
+  has_one_attached :photo
+  has_many :movies, through: :bookmarks, dependent: :destroy
+  has_many :bookmarks, dependent: :destroy
+  has_many :reviews, dependent: :destroy
+
+  validates :name, presence: true, uniqueness: true
 end
